6608f210970c4f9edab0fb5aff70c2acfee7700d
Arman Ebrahimi <armanebrhm@gmail.com>
2023-10-17 18:03:58 +0200

    refactoring bootstrap to load data from h2 and local database

--- src/main/java/guru/springframework/bootstrap/DataLoader.java
+++ src/main/java/guru/springframework/bootstrap/DataLoader.java
@@ -6,11 +6,13 @@ import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 @Component
 @Slf4j
+@Profile("default")
 public class DataLoader implements CommandLineRunner {
     private final UnitOfMeasureRepository unitOfMeasureRepository ;
     private final CategoryRepository categoryRepository;
